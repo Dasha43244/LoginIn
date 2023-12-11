b4c451409b0b95bc0c6795337227185ebdbddc76
GoodNight <129828409+Dasha43244@users.noreply.github.com>
2023-12-11 10:56:57 +0700

    Add files via upload

---
+++ Authorization/Login.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Authorization
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.LbLogin = new System.Windows.Forms.Label();
+            this.Player1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.BackColor = System.Drawing.Color.LightSalmon;
+            this.ExitBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Coral;
+            this.ExitBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ExitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ExitBtn.Location = new System.Drawing.Point(12, 457);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(100, 32);
+            this.ExitBtn.TabIndex = 7;
+            this.ExitBtn.Text = "Выйти";
+            this.ExitBtn.UseVisualStyleBackColor = false;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // LbLogin
+            // 
+            this.LbLogin.AutoSize = true;
+            this.LbLogin.BackColor = System.Drawing.Color.Transparent;
+            this.LbLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LbLogin.Location = new System.Drawing.Point(134, 24);
+            this.LbLogin.Name = "LbLogin";
+            this.LbLogin.Size = new System.Drawing.Size(48, 20);
+            this.LbLogin.TabIndex = 8;
+            this.LbLogin.Text = "Login";
+            // 
+            // Player1
+            // 
+            this.Player1.Enabled = true;
+            this.Player1.Location = new System.Drawing.Point(0, 0);
+            this.Player1.Name = "Player1";
+            this.Player1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player1.OcxState")));
+            this.Player1.Size = new System.Drawing.Size(960, 540);
+            this.Player1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Авторизирован:";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 501);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LbLogin);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.Player1);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Login_FormClosed);
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer Player1;
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.Label LbLogin;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Authorization/Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+
+
+namespace Authorization
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //Windows Media Player на форме для проигрования заставки при авторизации
+            Player1.URL = "Video.mp4";
+            Player1.uiMode = "none";
+            Player1.settings.playCount = 1000;
+            Player1.settings.autoStart = true;
+            Player1.enableContextMenu = false;
+            Player1.Enabled = false;
+        
+        }
+
+        //Выход из данного окна,на окно авторизации/регистрации
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {   Authorization form = new Authorization();
+            form.Show();
+            this.Close();
+        }
+
+        public void ChangeLabel(string s)
+        {
+            LbLogin.Text = s;
+        }
+        private void Login_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
